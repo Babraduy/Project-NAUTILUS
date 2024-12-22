979ba11b4a9b0494a94182718c480507bc3ef61e
Babraduy <romansuccessful@gmail.com>
2024-12-22 16:06:22 +0100

    It doesn't even matter how hard you try

--- main.cpp
+++ main.cpp
@@ -9,17 +9,23 @@ int main()
 	InitWindow(WIDTH, HEIGHT, "Project NAUTILUS");
 	SetTargetFPS(144);
 
+	// Load textures
+	Image i_scroll = LoadImage("img/scroll.png");
+
+	Texture2D scroll = LoadTextureFromImage(i_scroll);
+
 	while (!WindowShouldClose())
 	{
 		BeginDrawing();
 
 		ClearBackground(BLACK);
 
-		DrawCircle(WIDTH / 2, HEIGHT / 2, 50, WHITE);
+		DrawTexturePro(scroll, {0,0,(float)scroll.width,(float)scroll.height}, { WIDTH/2, HEIGHT/2,(float)scroll.width * 5.0f, (float)scroll.height * 5.0f },{ (float)scroll.width/2.0f, (float)scroll.height/2.0f}, 0, WHITE);
 
 		EndDrawing();
 	}
 
+	UnloadTexture(scroll);
 	CloseWindow();
 
 	return 0;
