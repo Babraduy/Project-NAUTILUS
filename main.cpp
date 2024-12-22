e0a2334a1908f1ac7563f9e8ab550f00f6f42016
Babraduy <romansuccessful@gmail.com>
2024-12-22 15:40:04 +0100

    One thing, i don't know why...

--- main.cpp
+++ main.cpp
@@ -1,10 +1,26 @@
 #include <iostream>
-#include <raylib.h>
+#include "raylib.h"
+
+#define WIDTH 800
+#define HEIGHT 600
 
 int main()
 {
-	InitWindow(800, 600, "Project NAUTILUS");
+	InitWindow(WIDTH, HEIGHT, "Project NAUTILUS");
 	SetTargetFPS(144);
 
+	while (!WindowShouldClose())
+	{
+		BeginDrawing();
 
+		ClearBackground(BLACK);
+
+		DrawCircle(WIDTH / 2, HEIGHT / 2, 50, WHITE);
+
+		EndDrawing();
+	}
+
+	CloseWindow();
+
+	return 0;
 }
